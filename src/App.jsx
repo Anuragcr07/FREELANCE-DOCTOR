99dc89cde2c81fbe4cbd2772db05da0c0d72bb02
Anuragcr07 <anurag8081482979@gmail.com>
2025-07-18 23:36:27 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen w-screen bg-cover bg-center bg-[url('./assets/background.jpg')]">
+      <h1 className="text-3xl font-bold text-center text-white pt-10">
+        MEDICAL STORE MANAGEMENT
+      </h1>
+
+    
+    </div>
+  );
+}
+
+export default App;
