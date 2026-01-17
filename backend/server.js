96bb6422c35abbc30e55695fe5813727c0d9371f
ANURAG <159320626+Anuragcr07@users.noreply.github.com>
2026-01-17 18:35:01 +0530

    Update CORS origin for server configuration

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 const __dirname = path.resolve();
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://freelance-doctor-medical-01.onrender.com"],
+  origin: ["https://freelance-doctor-yfwt.onrender.com"],
   credentials: true,
   optionSuccessStatus: 200,
 };
